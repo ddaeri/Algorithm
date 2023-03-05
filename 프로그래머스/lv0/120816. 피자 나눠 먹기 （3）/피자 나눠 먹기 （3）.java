6990626882cb29756baf2e11312b33c6246382fa
ddaeri <100750346+ddaeri@users.noreply.github.com>
2023-03-05 23:11:28 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -5,6 +5,7 @@ class Solution {
         if(n % slice == 0) {
             answer = n / slice;
         } else {
+            // 0 으로 떨어지지 않으면 무조건 1판 더 시켜야하므로 + 1 해준다.
             answer = n / slice + 1;
         }
         return answer;
